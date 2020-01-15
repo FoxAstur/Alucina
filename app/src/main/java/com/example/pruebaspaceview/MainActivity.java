5efcf9a3d4df7ffbdcff8ddbd52dfb4968587c1d
Aadirn <adriangallego_98@hotmail.es>
2020-01-15 18:54:33 +0100

    Creados Fragments: ////////////////////////////////////////////// =>PREGUNTAS JORGE: -Conversion de iconos e imagenes mitmap a xml -Todo lo que no entiendo de cosas que me puso solo el AS -Continuidad del contexto en un fragment ////////////////////////////////////////////// =>ERRORES: -El fragmento "HOME" me lo muestra bien, pero al cambiar al resto la aplicacion crashea (Puede deberse igual a la falta de contexto, o el no mantener continuo el SpaceViewer) //////////////////////////////////////////////

--- app/src/main/java/com/example/pruebaspaceview/MainActivity.java
+++ app/src/main/java/com/example/pruebaspaceview/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.pruebaspaceview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.ContentProvider;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
 
 import com.luseen.spacenavigation.SpaceItem;
 import com.luseen.spacenavigation.SpaceNavigationView;
 import com.luseen.spacenavigation.SpaceOnClickListener;
+
+import ui.fragmentBulbs.BulbFragment;
+import ui.fragmentHome.HomeFragment;
+import ui.fragmentSettings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,13 +29,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState==null){
+
+            FragmentManager manager = getSupportFragmentManager();
+
+            FragmentTransaction trans = manager.beginTransaction();
+            trans.replace(R.id.act_main_container_frame, HomeFragment.newInstance());
+            trans.commitNow();
+
+
+        }
         initComp(savedInstanceState);
     }
 
     private void initComp(Bundle savedInstanceState) {
 
 
-        SpaceNavigationView spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
+        SpaceNavigationView spaceNavigationView = (SpaceNavigationView) findViewById(R.id.act_main_spaceViewer);
         spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
 
         SpaceItem itmInicio = new SpaceItem("INICIO", R.drawable.ic_action_home);
@@ -41,14 +55,32 @@ public class MainActivity extends AppCompatActivity {
         SpaceItem itmAjustes = new SpaceItem("AJUSTES", R.drawable.ic_action_settings);
         spaceNavigationView.addSpaceItem(itmAjustes);
 
+        spaceNavigationView.showIconOnly();
+
         spaceNavigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
             @Override
             public void onCentreButtonClick() {
                 //PONER LO QUE QUIERO QUE HAGA EL BOTON CENTRAL
+
+                bulbCall();
+
             }
 
             @Override
             public void onItemClick(int itemIndex, String itemName) {
+
+
+                if(itemIndex<=0){
+                    //Boton HOME
+
+                    homeCall();
+
+                }else{
+                    //Boton SETTINGS
+
+                    settingsCall();
+
+                }
 
             }
 
@@ -57,5 +89,35 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void homeCall() {
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        FragmentTransaction trans = manager.beginTransaction();
+        trans.replace(R.id.act_main_container_frame,new HomeFragment(),"home_01");
+        trans.commitNow();
+
+    }
+
+    private void settingsCall() {
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        FragmentTransaction trans = manager.beginTransaction();
+        trans.replace(R.id.act_main_container_frame,new SettingsFragment(),"sett_01");
+        trans.commitNow();
+
+    }
+
+    private void bulbCall() {
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        FragmentTransaction trans = manager.beginTransaction();
+        trans.replace(R.id.act_main_container_frame,new BulbFragment(),"bulb_01");
+        trans.commitNow();
+
     }
 }
